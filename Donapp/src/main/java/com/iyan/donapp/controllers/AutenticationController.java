81de90521c363820bddcc18401ef6f44249aa686
Iyán Sanjurjo <71704577+iyansl@users.noreply.github.com>
2024-02-16 21:00:53 +0100

    Update AutenticationController.java

--- Donapp/src/main/java/com/iyan/donapp/controllers/AutenticationController.java
+++ Donapp/src/main/java/com/iyan/donapp/controllers/AutenticationController.java
@@ -5,39 +5,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.iyan.donapp.model.dto.UserRegistroDto;
 import com.iyan.donapp.services.UserService;
 
 @Controller
-@RequestMapping("/registrarse")
 public class AutenticationController {
 
-	@Autowired
-	private UserService userService;
-	
-	@ModelAttribute("user")
-	public UserRegistroDto nuevoUserRegistroDto() {
-		return new UserRegistroDto();
-	}
+    @Autowired
+    private UserService userService;
+    
+    @ModelAttribute("user")
+    public UserRegistroDto nuevoUserRegistroDto() {
+        return new UserRegistroDto();
+    }
 
-	@PostMapping
-	public String registrarse(@ModelAttribute("user") UserRegistroDto dto) {
-		if (userService.getUserByEmail(dto.getEmail()) != null) 
-			return "redirect:/registrarse?error1";
-		else if (userService.getUserByUsername(dto.getUsername()) != null) 
-			return "redirect:/registrarse?error";
-		else 
-			userService.saveUser(dto);
-		return "redirect:/iniciarsesion?exito";
-	}
-	
-	@GetMapping
-	public String mostrarRegistro() {
-		return "/registrarse";
-	}
+    @GetMapping("/registrarse")
+    public String mostrarRegistro() {
+        return "registrarse";
+    }
 
-	
-
+    @PostMapping("/registrarse")
+    public String registrarse(@ModelAttribute("user") UserRegistroDto dto) {
+        if (userService.getUserByEmail(dto.getEmail()) != null) {
+            return "redirect:/registrarse?error1";
+        } else if (userService.getUserByUsername(dto.getUsername()) != null) {
+            return "redirect:/registrarse?error";
+        } else {
+            userService.saveUser(dto);
+            return "redirect:/iniciarsesion?exito";
+        }
+    }
 }
